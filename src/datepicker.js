bd2340f04dd8b967b641dc97de0730000b5b23a7
Nick Bittar <nickcb7@gmail.com>
2017-08-26 00:14:36 -0400

    Updated datepicker.  Fixed compatibility issues with IE.

--- src/datepicker.js
+++ src/datepicker.js
@@ -1,9 +1,14 @@
+'use strict';
 
 var datepicker = {
 	inputElem: null,
 	container: null,
 	datepicker: null,
 	focusFlag: false,
+	
+	inputInFocus: false,
+	datepickerInFocus: false,
+	
 	viewMode: 'day',	// day, month, year, decade
 	options: {
 		defaultDate: new Date(),
@@ -14,6 +19,7 @@ var datepicker = {
 		hideCalendarOnSelect: true,
 		todayButton: true,
 		darkMode: false,
+		compact: true,
 	},
 	dateRegex: /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
 	
@@ -23,6 +29,8 @@ var datepicker = {
 	},
 	
 	init: function init(inputElem, options) {
+		var _this = this;
+		
 		this.inputElem = inputElem;
 		
 		// options
@@ -54,36 +62,48 @@ var datepicker = {
 			if(options.darkMode != undefined) {
 				this.options.darkMode = options.darkMode;
 			}
+			if(options.compact != undefined) {
+				this.options.compact = options.compact;
+			}
 		}
 		
 		this.container = document.createElement('div');
 		this.container.classList.add('datepicker-container');
+		this.container.tabIndex = 0;
 		if(this.options.darkMode) {
 			this.container.classList.add('dark');
+		}
+		if(this.options.compact) {
+			this.container.classList.add('compact');
 		}
 		
 		this.inputElem.insertAdjacentElement('afterend', this.container);
 		
-		this.inputElem.addEventListener('focus', event => this.updateCalendar(null));
-		
-		this.inputElem.addEventListener('blur', event => {
-			if(this.options.hideCalendarOnClickOff) 
-				setTimeout((event) => this.closeCalendar(event), 50);
+		this.inputElem.addEventListener('focus', function(event) {
+			_this.inputInFocus = true;
+			_this.updateCalendar(null);
 		});
-		this.container.addEventListener('blur', event => this.closeCalendar(event));
-		this.container.addEventListener('focus', event => this.focusFlag = true );
 		
-		this.inputElem.addEventListener('change', event => {
-			if(this.inputElem.value) {
-				if(!this.dateRegex.test(this.inputElem.value) || new Date(this.inputElem.value) < this.options.minDate || new Date(this.inputElem.value) > this.options.maxDate) {
-					this.inputElem.value = '';
+		this.inputElem.addEventListener('blur', function(event){
+			_this.inputInFocus = false;
+			if(_this.options.hideCalendarOnClickOff) 
+				setTimeout(function(event) { _this.closeCalendar(event); }, 100);
+		});
+		this.container.addEventListener('blur', function(event) { _this.datepickerInFocus = false; setTimeout(function() { _this.closeCalendar(event);}, 100); });
+		this.container.addEventListener('focus', function(event) { _this.datepickerInFocus = true; });
+		this.container.addEventListener('mousedown', function(event) { setTimeout(function() {_this.container.focus();}, 50); });
+		
+		this.inputElem.addEventListener('change', function(event) {
+			if(_this.inputElem.value) {
+				if(!_this.dateRegex.test(_this.inputElem.value) || new Date(_this.inputElem.value) < _this.options.minDate || new Date(_this.inputElem.value) > _this.options.maxDate) {
+					_this.inputElem.value = '';
 				}
 			}
 		});
 		
-		this.inputElem.addEventListener('input', event => {
-			let parts = this.inputElem.value.split('/');
-			let guess = this.options.defaultDate;
+		this.inputElem.addEventListener('input', function(event) {
+			let parts = _this.inputElem.value.split('/');
+			let guess = _this.options.defaultDate;
 			
 			if(parts[0] != undefined && parts[0] != '' && !isNaN(parts[0])) {
 				let month = parseInt(parts[0]);
@@ -113,7 +133,7 @@ var datepicker = {
 				}
 			}
 			
-			this.updateCalendar(guess);
+			_this.updateCalendar(guess);
 		});
 		
 		
@@ -173,26 +193,28 @@ var datepicker = {
 		
 		this.container.id = 'nb-datepicker-' + this.inputElem.id;
 		this.container.innerHTML = '';
-		this.container.tabIndex = 0;
 		
-		this.container.style.top = (this.inputElem.offsetTop + this.inputElem.offsetHeight + 5) + 'px';
-		this.container.style.left = this.inputElem.offsetLeft + 'px';
-		
+		this.repositionCalendar();
 		
 		this.datepicker = html;
 		this.container.appendChild(html);
 		
 	},
+	
+	repositionCalendar: function repositionCalendar() {
+		this.container.style.top = (this.inputElem.offsetTop + this.inputElem.offsetHeight + 5) + 'px';
+		this.container.style.left = this.inputElem.offsetLeft + 'px';
+	},
 
 	closeCalendar: function closeCalendar(event) {
-		if(!this.focusFlag) {
-			this.datepicker.remove();
-		} else {
-			this.focusFlag = false;
+		if(!(this.inputInFocus || this.datepickerInFocus) && this.datepicker.parentNode) {
+			this.datepicker.parentNode.removeChild(this.datepicker);
 		}	
 	},
 		
 	generateMonthHtml: function generateMonthHtml(dates, selectedDate) {
+		var _this = this;
+		
 		var currMonth = new Date(dates[15].date);
 		var prevMonth = this.utils.addMonths(currMonth, -1);
 		var nextMonth = this.utils.addMonths(currMonth, 1);
@@ -210,7 +232,7 @@ var datepicker = {
 				prevMonthBtn.classList.add('calendar-header-control');
 				prevMonthBtn.classList.add('calendar-header-label');
 				prevMonthBtn.title = 'Go to previous month: ' + this.utils.getMonthName(prevMonth) + ' ' + prevMonth.getFullYear();
-				prevMonthBtn.addEventListener('click', event =>  this.updateCalendar(prevMonth) );
+				prevMonthBtn.addEventListener('click', function(event) { _this.updateCalendar(prevMonth); });
 				prevMonthBtn.innerHTML = '&#171;';
 				calendarHeader.appendChild(prevMonthBtn);
 
@@ -220,9 +242,9 @@ var datepicker = {
 				currMonthLabel.classList.add('calendar-header-label');
 				currMonthLabel.title = 'Go to month selection';
 				currMonthLabel.innerHTML = dates[15].date.getFullYear() + '<br>' + this.utils.getMonthName(dates[15].date);
-				currMonthLabel.addEventListener('click', event => {
-					this.changeViewMode('month', dates[15].date);
-					this.viewMode = 'month';
+				currMonthLabel.addEventListener('click', function(event) {
+					_this.changeViewMode('month', dates[15].date);
+					_this.viewMode = 'month';
 				});
 				calendarHeader.appendChild(currMonthLabel);
 				
@@ -231,7 +253,7 @@ var datepicker = {
 				nextMonthBtn.classList.add('calendar-header-control');
 				nextMonthBtn.classList.add('calendar-header-label');
 				nextMonthBtn.title = 'Go to next month: ' + this.utils.getMonthName(nextMonth) + ' ' + nextMonth.getFullYear();
-				nextMonthBtn.addEventListener('click', event => this.updateCalendar(nextMonth) );
+				nextMonthBtn.addEventListener('click', function(event) { _this.updateCalendar(nextMonth); });
 				nextMonthBtn.innerHTML = '&#187;';
 				calendarHeader.appendChild(nextMonthBtn);
 			
@@ -249,25 +271,27 @@ var datepicker = {
 				}
 				
 				for(let i = 0; i < dates.length; i++) {
+					let currDate = dates[i];
+					
 					let calendarDate = document.createElement('div');
 					calendarDate.classList.add('calendar-item');
 					calendarDate.classList.add('calendar-date');
 					calendarDate.classList.add('calendar-option');
-					if(selectedDate && this.utils.toISODateFormat(selectedDate) == this.utils.toISODateFormat(dates[i].date)) calendarDate.classList.add('selected-date');
-					calendarDate.classList.add((dates[i].currentMonth ? 'current-month' : 'not-current-month' ));
-					calendarDate.title = this.utils.toNormalDateFormat(dates[i].date);
-					calendarDate.setAttribute('data-date-value', this.utils.toISODateFormat(dates[i].date));
+					if(selectedDate && this.utils.toISODateFormat(selectedDate) == this.utils.toISODateFormat(currDate.date)) calendarDate.classList.add('selected-date');
+					calendarDate.classList.add((currDate.currentMonth ? 'current-month' : 'not-current-month' ));
+					calendarDate.title = this.utils.toNormalDateFormat(currDate.date);
+					calendarDate.setAttribute('data-date-value', this.utils.toISODateFormat(currDate.date));
 					
-					calendarDate.innerText = dates[i].date.getDate();
-					if(this.options.minDate && dates[i].date < this.options.minDate ) {
+					calendarDate.innerText = currDate.date.getDate();
+					if(this.options.minDate && currDate.date < this.options.minDate ) {
 						calendarDate.classList.add('calendar-item-disabled');
 					}
-					if(this.options.maxDate && dates[i].date > this.options.maxDate ) {
+					if(this.options.maxDate && currDate.date > this.options.maxDate ) {
 						calendarDate.classList.add('calendar-item-disabled');
 					}
 					
 					if(!calendarDate.classList.contains('calendar-item-disabled')) {
-						calendarDate.addEventListener('click', event => this.pickDate(event, new Date(dates[i].date), false));
+						calendarDate.addEventListener('click', function(event) { _this.pickDate(event, currDate.date, false); });
 						calendarDate.classList.add('calendar-date-option');
 					} else {
 						calendarDate.classList.remove('calendar-option');
@@ -281,6 +305,8 @@ var datepicker = {
 	
 		
 	generateYearHtml: function generateYearHtml(dates, selectedDate) {
+		var _this = this;
+		
 		var currYear = new Date(dates[0].date);
 		var prevYear = this.utils.addMonths(currYear, -12);
 		var nextYear = this.utils.addMonths(currYear, 12);
@@ -298,7 +324,7 @@ var datepicker = {
 				prevYearBtn.classList.add('calendar-header-control');
 				prevYearBtn.classList.add('calendar-header-label');
 				prevYearBtn.title = 'Go to previous year: ' + prevYear.getFullYear();
-				prevYearBtn.addEventListener('click', event =>  this.updateCalendar(prevYear) );
+				prevYearBtn.addEventListener('click', function(event) { _this.updateCalendar(prevYear); });
 				prevYearBtn.innerHTML = '&#171;';
 				calendarHeader.appendChild(prevYearBtn);
 
@@ -308,8 +334,8 @@ var datepicker = {
 				currYearLabel.classList.add('calendar-header-label');
 				currYearLabel.title = 'Go to year selection';
 				currYearLabel.innerHTML = dates[0].date.getFullYear();
-				currYearLabel.addEventListener('click', event => {
-					this.changeViewMode('year', dates[0].date);
+				currYearLabel.addEventListener('click', function(event) {
+					_this.changeViewMode('year', dates[0].date);
 				});
 				calendarHeader.appendChild(currYearLabel);
 				
@@ -318,7 +344,7 @@ var datepicker = {
 				nextYearBtn.classList.add('calendar-header-control');
 				nextYearBtn.classList.add('calendar-header-label');
 				nextYearBtn.title = 'Go to next year: ' + nextYear.getFullYear();
-				nextYearBtn.addEventListener('click', event => this.updateCalendar(nextYear) );
+				nextYearBtn.addEventListener('click', function(event) { _this.updateCalendar(nextYear); });
 				nextYearBtn.innerHTML = '&#187;';
 				calendarHeader.appendChild(nextYearBtn);
 			
@@ -327,26 +353,28 @@ var datepicker = {
 			calendar.appendChild(calendarBody);
 				
 				for(let i = 0; i < dates.length; i++) {
+					let currDate = dates[i];
+					
 					let calendarMonth = document.createElement('div');
 					calendarMonth.classList.add('calendar-item');
 					calendarMonth.classList.add('calendar-month');
 					calendarMonth.classList.add('calendar-option');
-					if(selectedDate && this.utils.toISODateFormat(this.utils.getFirstDayOfTheMonth(selectedDate)) == this.utils.toISODateFormat(this.utils.getFirstDayOfTheMonth(dates[i].date))) calendarMonth.classList.add('selected-date');
-					calendarMonth.title = this.utils.getMonthName(dates[i].date) + ' ' + dates[i].date.getFullYear();
-					calendarMonth.setAttribute('data-date-value', this.utils.toISODateFormat(dates[i].date));
-					calendarMonth.innerText = this.utils.getMonthName(dates[i].date).slice(0, 3);
+					if(selectedDate && this.utils.toISODateFormat(this.utils.getFirstDayOfTheMonth(selectedDate)) == this.utils.toISODateFormat(this.utils.getFirstDayOfTheMonth(currDate.date))) calendarMonth.classList.add('selected-date');
+					calendarMonth.title = this.utils.getMonthName(currDate.date) + ' ' + currDate.date.getFullYear();
+					calendarMonth.setAttribute('data-date-value', this.utils.toISODateFormat(currDate.date));
+					calendarMonth.innerText = this.utils.getMonthName(currDate.date).slice(0, 3);
 					let minMonth = this.utils.getFirstDayOfTheMonth(this.options.minDate);
-					if(this.options.minDate && dates[i].date < minMonth ) {
+					if(this.options.minDate && currDate.date < minMonth ) {
 						calendarMonth.classList.add('calendar-item-disabled');
 					}
 					let maxMonth = this.utils.addMonths(this.utils.getFirstDayOfTheMonth(this.options.maxDate), 0);
-					if(this.options.maxDate && dates[i].date > maxMonth ) {
+					if(this.options.maxDate && currDate.date > maxMonth ) {
 						calendarMonth.classList.add('calendar-item-disabled');
 					}
 					
 					if(!calendarMonth.classList.contains('calendar-item-disabled')) {
-						calendarMonth.addEventListener('click', event => {
-							this.changeViewMode('day', dates[i].date);
+						calendarMonth.addEventListener('click', function(event) {
+							_this.changeViewMode('day', currDate.date);
 						});
 						calendarMonth.classList.add('calendar-month-option');
 					} else {
@@ -361,6 +389,8 @@ var datepicker = {
 	
 	
 	generateDecadeHtml: function generateDecadeHtml(dates, selectedDate) {
+		var _this = this;
+		
 		var currDecade = new Date(dates[0].date);
 		var prevDecade = this.utils.addMonths(currDecade, -12*10);
 		var nextDecade = this.utils.addMonths(currDecade, 12*10);
@@ -378,7 +408,7 @@ var datepicker = {
 				prevDecadeBtn.classList.add('calendar-header-control');
 				prevDecadeBtn.classList.add('calendar-header-label');
 				prevDecadeBtn.title = 'Go to previous decade: ' + prevDecade.getFullYear();
-				prevDecadeBtn.addEventListener('click', event =>  this.updateCalendar(prevDecade) );
+				prevDecadeBtn.addEventListener('click', function(event) { _this.updateCalendar(prevDecade); });
 				prevDecadeBtn.innerHTML = '&#171;';
 				calendarHeader.appendChild(prevDecadeBtn);
 
@@ -396,7 +426,7 @@ var datepicker = {
 				nextDecadeBtn.classList.add('calendar-header-control');
 				nextDecadeBtn.classList.add('calendar-header-label');
 				nextDecadeBtn.title = 'Go to next decade: ' + nextDecade.getFullYear();
-				nextDecadeBtn.addEventListener('click', event => this.updateCalendar(nextDecade) );
+				nextDecadeBtn.addEventListener('click', function(event) { _this.updateCalendar(nextDecade); });
 				nextDecadeBtn.innerHTML = '&#187;';
 				calendarHeader.appendChild(nextDecadeBtn);
 			
@@ -404,28 +434,30 @@ var datepicker = {
 			calendarBody.classList.add('calendar-body');
 			calendar.appendChild(calendarBody);
 				
-				for(let i = 0; i < dates.length; i++) {
+				for(var i = 0; i < dates.length; i++) {
+					let currDate = dates[i];
+					
 					let calendarYear = document.createElement('div');
 					calendarYear.classList.add('calendar-item');
 					calendarYear.classList.add('calendar-year');
 					calendarYear.classList.add('calendar-option');
-					if(selectedDate && selectedDate.getFullYear() == dates[i].date.getFullYear()) calendarYear.classList.add('selected-date');
-					calendarYear.title = dates[i].date.getFullYear();
-					calendarYear.setAttribute('data-date-value', this.utils.toISODateFormat(dates[i].date));
-					calendarYear.innerText = dates[i].date.getFullYear();
+					if(selectedDate && selectedDate.getFullYear() == currDate.date.getFullYear()) calendarYear.classList.add('selected-date');
+					calendarYear.title = currDate.date.getFullYear();
+					calendarYear.setAttribute('data-date-value', this.utils.toISODateFormat(currDate.date));
+					calendarYear.innerText = currDate.date.getFullYear();
 					
 					let minYear = this.options.minDate.getFullYear();
-					if(this.options.minDate && dates[i].date.getFullYear() < minYear ) {
+					if(this.options.minDate && currDate.date.getFullYear() < minYear ) {
 						calendarYear.classList.add('calendar-item-disabled');
 					}
 					let maxYear = this.options.maxDate.getFullYear();
-					if(this.options.maxDate && dates[i].date.getFullYear() > maxYear ) {
+					if(this.options.maxDate && currDate.date.getFullYear() > maxYear ) {
 						calendarYear.classList.add('calendar-item-disabled');
 					}
 					
 					if(!calendarYear.classList.contains('calendar-item-disabled')) {
-						calendarYear.addEventListener('click', event => {
-							this.changeViewMode('month', dates[i].date);
+						calendarYear.addEventListener('click', function(event) {
+							_this.changeViewMode('month', currDate.date);
 						});
 						calendarYear.classList.add('calendar-year-option');
 					} else {
@@ -439,13 +471,15 @@ var datepicker = {
 	},
 	
 	generateTodayButton: function generateTodayButton() {
+		var _this = this;
+		
 		let todayButton = document.createElement('div');
 		todayButton.classList.add('calendar-today-button');
 		todayButton.title = 'Go to today (' + this.utils.toNormalDateFormat(new Date()) + ')';
 		todayButton.innerText = 'Today';
-		todayButton.addEventListener('click', event => {
-			this.viewMode = 'day';	// Maybe change this to check min view mode
-			this.pickDate(event, new Date(), true);
+		todayButton.addEventListener('click', function(event) {
+			_this.viewMode = 'day';	// Maybe change this to check min view mode
+			_this.pickDate(event, new Date(), true);
 		});
 		return todayButton;
 	},
@@ -455,7 +489,7 @@ var datepicker = {
 		this.inputElem.value = this.utils.toNormalDateFormat(date);
 		this.updateCalendar(date);
 		if(this.options.hideCalendarOnSelect && !keepOpen)
-			this.datepicker.remove();
+			this.datepicker.parentNode.removeChild(this.datepicker);
 	},
 	
 	getCalendarDates: function getCalendarDates(d) {
@@ -669,4 +703,5 @@ var datepicker = {
 	},
 	
 };
+
 
